cbaded03be6b61bad56b739549d8d27599c32bb0
Aviral Goel <goel.aviral@gmail.com>
2020-02-19 12:27:08 -0500

    Add environment variables for setting state and logging level

--- src/initialization.c
+++ src/initialization.c
@@ -1,6 +1,25 @@
 #include "library/initialization.h"
+#include <stdlib.h>
+#include <string.h>
 
 static int interceptr_wrappers_initialized = 0;
+
+enum interceptr_state_t {
+    INTERCEPTR_STATE_DEFAULT,
+    INTERCEPTR_STATE_FORCE_ENABLE,
+    INTERCEPTR_STATE_FORCE_DISABLE
+};
+
+enum interceptr_state_t interceptr_state;
+
+enum interceptr_log_level_t {
+    INTERCEPTR_LOG_LEVEL_DEBUG,
+    INTERCEPTR_LOG_LEVEL_INFO,
+    INTERCEPTR_LOG_LEVEL_WARN,
+    INTERCEPTR_LOG_LEVEL_ERROR
+};
+
+enum interceptr_log_level_t interceptr_log_level;
 
 void interceptr_initialize_wrappers() {
     if (interceptr_wrappers_initialized) {
@@ -11,6 +30,27 @@ void interceptr_initialize_wrappers() {
     initialize_stdlib_wrappers();
     initialize_unistd_wrappers();
     initialize_sys_stat_wrappers();
+
+    const char* value = getenv("INTERCEPTR_STATE");
+    if (value == NULL || strcmp(value, "default") == 0) {
+        interceptr_state = INTERCEPTR_STATE_DEFAULT;
+    } else if (strcmp(value, "force-enable") == 0) {
+        interceptr_state = INTERCEPTR_STATE_FORCE_ENABLE;
+    } else if (strcmp(value, "force-disable") == 0) {
+        interceptr_state = INTERCEPTR_STATE_FORCE_DISABLE;
+    }
+
+    value = getenv("INTERCEPTR_LOG_LEVEL");
+    if (value == NULL || (strcmp(value, "error") == 0)) {
+        interceptr_log_level = INTERCEPTR_LOG_LEVEL_ERROR;
+    } else if (strcmp(value, "info") == 0) {
+        interceptr_log_level = INTERCEPTR_LOG_LEVEL_INFO;
+    } else if (strcmp(value, "warn") == 0) {
+        interceptr_log_level = INTERCEPTR_LOG_LEVEL_WARN;
+    } else if (strcmp(value, "debug") == 0) {
+        interceptr_log_level = INTERCEPTR_LOG_LEVEL_DEBUG;
+    }
+
     interceptr_wrappers_initialized = 1;
 }
 
